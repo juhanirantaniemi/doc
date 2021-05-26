75f1cae29779724b5ec9e8af89132f77a02e3034
Juhani <juhani.rantaniemi@gmail.com>
2021-05-26 22:51:21 +0300

    prims works

--- prims.py
+++ prims.py
@@ -1,24 +1,25 @@
-
 import random
 import math
 import numpy as np
 import time as t
+import networkx as nx
+import matplotlib.pyplot as plt
 
 print("started")
 t0 = t.time()
 #Create random nodes
-coordinates = {}
-coords = [] # {n: ((x, y), distance, radius)}
-node_count = 1000
-d = {}
+formulas = []
+pos = {}# {n: ( distance, radius)}
+node_count = 100
 n = 0
 #Section is a bit slow
-while len(d) < node_count:
+while len(pos) < node_count:
     x, y = int(random.random()*50), int(random.random()*50)
-    if (x, y) not in d:
-        coords.append([n, (x, y), math.sqrt(x**2 + y**2), math.atan2(y,x)])
-        d[(x, y)] = True
+    if (x, y) not in pos:
+        formulas.append([n, math.sqrt(x**2 + y**2), math.atan2(y,x)])
+        pos[(x, y)] = n
         n += 1
+pos = {n: coord for coord, n in pos.items()}
 
 #Create matrix between distances of nodes
 
@@ -30,8 +31,8 @@ def return_distance(d1, d2, r1, r2):
 
 #Create weights and distances
 W = np.zeros((node_count, node_count))
-for i, coord, d1, r1  in coords:
-    for j, coord, d2, r2 in coords[i+1:]:
+for i, d1, r1  in formulas:
+    for j, d2, r2 in formulas[i+1:]:
         W[i,j] = W[j][i] =  round(return_distance(d1, d2, r1, r2), 1)
 maxw = np.max(W)
 
@@ -47,18 +48,31 @@ while np.sum(D) == np.inf:
     #Set the new weight
 
     #Create mask with Trues on rows and replace False rows with INF
-    no_go_rows = ((np.nanmax(E, axis=1) == 0).T * ones).T #The picked row indicates to known node and column for the new
-    W_next = W + ((E + no_go_rows) * maxw) #Make sure that already picked ones and not available rows are not the min
-    minw = np.min(W_next) 
+    forbidded_parent_nodes = ((np.nanmax(E, axis=1) == 0).T * ones).T #The picked row indicates to known node and column for the new
+    forbidded_child_nodes = ((np.nanmax(E, axis=1) == 1).T * ones)
+    W_next = W + ((forbidded_parent_nodes + forbidded_child_nodes) * maxw)
+    minw = np.min(W_next)
     i, j = np.argwhere(W_next== minw)[0]
     D[j,:] = D[i,:] + minw #Update the row to the found new node
     D[:,j] = D[:,i] + minw #update the column to the found new node
     D[j][j] = 0
     E[i][j] = E[j][i] = E[j][j] = True
-print(W)
-print(D)
-print(E)
 print(t.time() - t0)
+
+G = nx.from_numpy_matrix(E)
+nx.draw(G, pos=pos, with_labels=True, font_weight='bold')
+plt.show()
+
+# Calculate status quo: Optimal location
+    # Select the maximum node
+    # Calculate consumption and lossess
+    # 
+# Calculate loss-aware optimum
+    # Select minimum per row for E - W*lossess
+    # Calculate consumption and lossess
+
+#Find percentage where loss-aware and location-only are the same.
+
 
 
 #https://stackoverflow.com/questions/44360084/multiplying-numpy-2d-array-with-1d-array
